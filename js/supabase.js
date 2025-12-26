57db554ed75a1e63c6f31d014ffe266c6ac11b12
juliodantaspmrn <juliodantascontato@gmail.com>
2025-12-25 21:46:20 -0300

    Delete js/supabase.js

--- js/supabase.js
+++
@@ -1,9 +0,0 @@
-const SUPABASE_URL = "https://pucnyegqdiuzscezzugx.supabase.co";
-
-const SUPABASE_ANON_KEY =
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1Y255ZWdxZGl1enNjZXp6dWd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYzMzU1MjYsImV4cCI6MjA4MTkxMTUyNn0.7LTcIyPOwqX1Z2UeVumAJBP6buWz3du3oNRCpVwvpiY";
-
-const supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
